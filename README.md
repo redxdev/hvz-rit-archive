1bedb88e10733fdd5bd2bdde41cbe7075e66afb6
Sam Bloomberg <sam@redxdev.com>
2017-07-05 19:50:43 -0700

    working archive, first game

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# hvz-rit-archive
-Archive Source for HvZ @ RIT
+# Humans vs Zombies @ RIT Archive
+
+This is the source for the archive at https://hvz.rit.edu/archive.
+
+Created by [Sam Bloomberg](https://github.com/redxdev).
+
+All data and code under the [CC BY-NC 4.0](https://creativecommons.org/licenses/by-nc/4.0/)
---
+++ app.js
@@ -0,0 +1,87 @@
+// This code is quick + dirty, pls no h8
+
+var games = [];
+var gameTemplate = null;
+
+function registerGame(gameId, gameName) {
+    games.push({id: gameId, name: gameName});
+}
+
+function renderIndex() {
+    var container = document.createElement("div");
+
+    var title = document.createElement("h2");
+    title.innerHTML = "Archive Index";
+    title.className = "text-center";
+    container.appendChild(title);
+
+    var items = document.createElement("ul");
+    container.appendChild(items);
+
+    games.forEach(function (el) {
+        var textContainer = document.createElement("li");
+        items.appendChild(textContainer);
+
+        var text = document.createElement("a");
+        text.href = "#";
+        text.onclick = function () {
+            displayGame(el);
+        };
+        text.innerText = el.name;
+        textContainer.appendChild(text);
+    });
+
+    return container;
+}
+
+function resetContainers() {
+    $('#index-container').empty();
+    $('#content-container').empty();
+}
+
+function displayIndex() {
+    resetContainers();
+
+    $('#index-container').append(renderIndex());
+}
+
+function displayGame(game) {
+    resetContainers();
+
+    $.getJSON("games/" + game.id + ".json", function (data) {
+        data.name = game.name;
+        data.id = game.id;
+
+        data.missions.forEach(function (mission) {
+            switch (mission.team) {
+            case "all":
+                mission.all = true;
+                break;
+
+            case "zombie":
+                mission.zombie = true;
+                break;
+
+            case "human":
+                mission.human = true;
+                break;
+            }
+        });
+
+        var html = gameTemplate(data);
+        $('#content-container').html(html);
+    })
+    .fail (function(err) {
+        console.error(err);
+        $('#index-container').html('<h2>Unable to load game</h2>');
+    });
+}
+
+$(function () {
+    registerGame("2015-fall", "2015 Fall");
+
+    var source = $('#content-container').html();
+    gameTemplate = Handlebars.compile(source);
+
+    displayIndex();
+});
